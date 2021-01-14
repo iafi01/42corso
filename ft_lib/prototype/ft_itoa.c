f2a333afdd28b517b4d0187f08012059da48b1f1
IAFI <67833440+iafi01@users.noreply.github.com>
2021-01-14 19:04:39 +0100

    Add files via upload

--- ft_lib/prototype/ft_itoa.c
+++ ft_lib/prototype/ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: liafigli <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/13 14:05:09 by liafigli          #+#    #+#             */
-/*   Updated: 2021/01/14 11:20:16 by liafigli         ###   ########.fr       */
+/*   Updated: 2021/01/14 18:01:33 by liafigli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,36 @@
 
 char	*ft_itoa(int n)
 {
+	int i;
+	char *str;
+	int sign;
+	int d;
+	int nb;
+
+	i = 1;
+	d = 1;
+	nb = n;
 	if (n < 0)
-		ft_putchar_fd('-', 1);
-	
+	{
+		sign = -1;
+		n *= -1;
+		++d;
+	}
+	while (n > 0)
+	{
+		n /= 10;
+		i++;
+	}
+	str = malloc(i + d);
+	str[i] = 0;
+	i--;
+	n = nb * sign;
+	while (n > 0)
+	{
+		str[i--] = n % 10 + '0';
+		n /= 10;
+	}
+	if (sign > 0)
+		str[i] == '-';
+	return (str);
 }
