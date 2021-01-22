3a081e70fb476fa8b71fe1b724397eae06d64d75
IAFI <67833440+iafi01@users.noreply.github.com>
2021-01-22 11:57:14 -0500

    Add files via upload

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,13 +6,38 @@
 /*   By: liafigli <liafigli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/21 18:13:11 by liafigli          #+#    #+#             */
-/*   Updated: 2021/01/21 18:51:01 by liafigli         ###   ########.fr       */
+/*   Updated: 2021/01/22 17:56:50 by liafigli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
+
+static int checknl(char *buf)
+{
+    int i;
+
+    i = 0;
+    while (buf[i])
+    {
+        if (buf[i] == '\n')
+        {
+            return (i);
+        }
+        i++;
+    }
+    return (-1);
+}
+
 int get_next_line(int fd, char **line)
 {
-    if ((fd < 0 && fd > 4098) || !line)
+    int count;
+    int i;
+    static char *arr;
+    char *buf[BUFFER_SIZE + 1];
+    if (BUFFER_SIZE <= 0 || fd < 0 || !line)
         return (-1);
-    
+    count = read(fd, buf, BUFFER_SIZE);
+    ft_substr(buf, );
+    i = checknl(buf);
+    arr = ft_strjoin(arr, buf);
 }
--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: liafigli <liafigli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/21 18:13:48 by liafigli          #+#    #+#             */
-/*   Updated: 2021/01/21 18:31:38 by liafigli         ###   ########.fr       */
+/*   Updated: 2021/01/22 17:52:25 by liafigli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,5 +20,8 @@
 # endif
 
 int     get_next_line(int fd, char **line);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+size_t  asdft_strlen(const char *str);
 
 #endif
