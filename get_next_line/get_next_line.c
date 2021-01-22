7af4b47b123932814a66b324515d8f2a8fa63ca8
IAFI <67833440+iafi01@users.noreply.github.com>
2021-01-22 12:45:19 -0500

    Add files via upload

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: liafigli <liafigli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/21 18:13:11 by liafigli          #+#    #+#             */
-/*   Updated: 2021/01/22 17:56:50 by liafigli         ###   ########.fr       */
+/*   Updated: 2021/01/22 18:44:23 by liafigli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,16 +28,45 @@ static int checknl(char *buf)
     return (-1);
 }
 
+static int countnl(char *buf)
+{
+    int i;
+    int n;
+
+    i = 0;
+    n = 0;
+    while (buf[i])
+    {
+        if (buf[i] == '\n')
+            n++;
+        i++;
+    }
+    return (n);
+}
+
 int get_next_line(int fd, char **line)
 {
     int count;
     int i;
+    int c;
+    static int start;
     static char *arr;
     char *buf[BUFFER_SIZE + 1];
+
+    start = 0;
     if (BUFFER_SIZE <= 0 || fd < 0 || !line)
         return (-1);
     count = read(fd, buf, BUFFER_SIZE);
-    ft_substr(buf, );
-    i = checknl(buf);
-    arr = ft_strjoin(arr, buf);
+    if (count == 1)
+        arr = ft_strjoin(arr, buf);
+    if (i = checknl(buf) > 0)
+    {
+        while (c = countnl(buf))
+        {
+            *line = ft_substr(arr, start, i);
+            start = i;
+            *line++;
+            c--;
+        }
+    }
 }
