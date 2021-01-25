d9357322ab95f997eb9a6bce6713f05976daf639
IAFI <67833440+iafi01@users.noreply.github.com>
2021-01-25 09:52:01 -0500

    Add files via upload

--- printf/ft_printf.c
+++ printf/ft_printf.c
@@ -6,12 +6,33 @@
 /*   By: liafigli <liafigli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/24 13:43:00 by liafigli          #+#    #+#             */
-/*   Updated: 2021/01/24 17:00:26 by liafigli         ###   ########.fr       */
+/*   Updated: 2021/01/25 11:26:54 by liafigli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft/libft.h"
 #include "ft_printf.h"
+
+void    flag(char c)
+{
+    
+    if (c == 'd')
+        ft_putnbr_fd(va_arg( ::::, int), 1);
+    if (c == 'c')
+        ft_putnbr_fd(va_arg( ::::, char), 1);
+    if (c == 's')
+        ft_putnbr_fd(va_arg( ::::, char*), 1);
+    if (c == 'u')
+        ft_putnbr_fd(va_arg( ::::, unsigned int), 1);
+    if (c == 'i')
+        ft_putnbr_fd(va_arg( ::::, int), 1);
+    if (c == 'x')
+        ft_putnbr_fd(va_arg( ::::, int), 1);
+    if (c == 'X')
+        ft_putnbr_fd(va_arg( ::::, int), 1);
+    if (c == 'p')
+        ft_putnbr_fd(va_arg( ::::, void *), 1);
+}
 
 int ft_printf(const char *fix, ...)
 {
@@ -20,8 +41,16 @@ int ft_printf(const char *fix, ...)
     va_start(ap, fix);
     while (*fix)
     {
-        if (*fix == '%')
-            
+        if (*fix++ == '%')
+        {
+            if (*fix == '%')
+                ft_putchar_fd('%', 1);
+            else
+                flag(*fix);
+        }
+        else
+            ft_putchar_fd(*fix, 1);
+        *fix++;
     }
     va_end(ap);
 }
